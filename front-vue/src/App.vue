9b86e1d35af78677755211254b8c9d0ce0a31edb
wagnerbsjunior <wagnerjf@gmail.com>
2021-02-13 11:23:08 -0300

    Intial commit

---
+++ front-vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+      <a class="navbar-brand order-md-last">Desafio CRUD</a>
+      <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#">
+        <i class="fas fa-bars"></i>
+    </button>
+    </nav>
+  </div>
+  <div class="container mt-5">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ test-vue/test-app/src/components/FormProfessorComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="justi">
+        
+    </div>
+</template>
